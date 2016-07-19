da88749f8097ac291104d02261821c91c5b5a35d
DevBaka <DevBaka@DESKTOP-NS8R3TV>
2016-07-19 12:38:39 +0200

    Aktueller stand

---
+++ init.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import url_for
+import os
+import sys
+
+
+app = Flask(__name__)
+wsgi_app = app.wsgi_app
+
+from routes import *
+
+if __name__ == '__main__':
+    HOST = os.environ.get("SERVER_HOST", "localhost")
+    try:
+        PORT = int(os.environ.get("SERVER_PORT", "4444"))
+    except ValueError:
+        PORT = 4444
+    app.run(HOST, PORT, debug=True)
+
+if sys.version_info.major < 3:
+    reload(sys)
+sys.setdefaultencoding('utf8')
---
+++ routes.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import url_for, redirect, render_template
+from init import app
+from misc.templating import templated
+
+def createNavbar(site):
+    foo = {
+        "index": ["about me", "kontakt"],
+        "about me": ["about me", "kontakt"],
+        "kontakt": ["about me", "kontakt"],
+        "Programmierung": ["Python", "C#", "C"]
+    }
+    return foo[site]
+
+
+@app.route("/")
+@app.route("/Start")
+@templated("Start.html")
+def index():
+    return {"seite": "index", "navbar": createNavbar}
+
+@app.route("/about me")
+@templated("about me.html")
+def aboutme():
+    return {"seite": "about me", "navbar": createNavbar}
+
+@app.route("/kontakt")
+@templated("start/kontakt.html")
+def kontakt():
+    return {"seite": "kontakt", "navbar": createNavbar}
+
+@app.route("/Programmierung")
+@templated("dev/Programmierung.html")
+def dev():
+    return {"seite": "Programmierung", "navbar": createNavbar}
+
+@app.route("/Kali Linux")
+@templated("kali/Kali Linux.html")
+def kalistart():
+    return {"seite": "Kali Linux", "navbar": createNavbar}
+
+@app.route("/dev/py2")
+def devpy():
+    return render_template("dev/python/py2.html")
+@app.route("/todo")
+def todo():
+    index = """
+    <h2> todo </h2>
+    <article>
+        <p> Die Aside soll wie eine TreeView sein. Alles was zu start gehört soll aufgeklappt werden.
+        Was zur Programmierung gehört soll aufgeklappt werden, sobald es angeklickt wird oder man im Header auf Programmierung klickt.
+        Darauf sollen Beispielsweise verschiedene Programmiersprachen aufgelistet werden.
+        Möglichkeiten:
+        <p>-menu/menuitems ... only html/css... contra: kann nit jeder Browser
+
+        <p>zweite möglichkeit:
+           <p> If clicked on Dev{include asidedev}...
+            <p>they have: Start, Elektrotechnik, Programming new since clicked-->Python, C#, C/C++, Lua, Pawn, Javascript, Kein Java usw...
+
+    </article>
+    """
+    return redirect("todo")
+
